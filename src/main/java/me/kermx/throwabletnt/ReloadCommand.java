8e615cf8ef0f82ffb464dc32bfc8a0160d5add9f
KermX <KermitVIII@gmail.com>
2023-04-12 21:48:54 -0500

    Initial commit

---
+++ src/main/java/me/kermx/throwabletnt/ReloadCommand.java
@@ -0,0 +1,2 @@
+package me.kermx.throwabletnt;public class ReloadCommand {
+}
---
+++ src/main/java/me/kermx/throwabletnt/ThrowableTNT.java
@@ -0,0 +1,17 @@
+package me.kermx.throwabletnt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThrowableTNT extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
